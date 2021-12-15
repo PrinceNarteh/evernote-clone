30a97d012705bf38494865b49be17cab8f840184
PrinceNarteh <prinarteh@gmail.com>
2021-12-15 04:37:08 +0000

    adds dotenv for environment variables

---
+++ server/src/constants/strings.ts
@@ -0,0 +1,5 @@
+import "dotenv/config";
+
+export const CONSTANTS = {
+  PORT: process.env.PORT,
+};
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,24 @@
+import cors from "cors";
+import express from "express";
+import morgan from "morgan";
 import "reflect-metadata";
 import { createConnection } from "typeorm";
-import express from "express";
-const PORT = process.env.PORT || 4000;
+import { CONSTANTS } from "./constants/strings";
 
 createConnection()
   .then(async (connection) => {
     const app = express();
 
+    // Express middlewares
+    app.use(cors());
+    app.use(morgan("dev"));
+
     app.get("/", (req, res) => {
       res.send("Hello, World!");
     });
 
-    app.listen(4000, () =>
-      console.log(`Server running on http://localhost:${PORT}`)
+    app.listen(CONSTANTS.PORT, () =>
+      console.log(`Server running on http://localhost:${CONSTANTS.PORT}`)
     );
   })
   .catch((error) => console.log(error));
