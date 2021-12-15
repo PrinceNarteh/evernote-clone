03c45a5f0c52eba08260c117eff8b7bfff56da58
PrinceNarteh <prinarteh@gmail.com>
2021-12-15 06:47:32 +0000

    setup apollo server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,14 @@
+import { ApolloServer } from "apollo-server-express";
 import cors from "cors";
 import express from "express";
 import morgan from "morgan";
 import "reflect-metadata";
 import { createConnection } from "typeorm";
 import { CONSTANTS } from "./constants/strings";
+import typeDefs from "./graphql/typeDefs";
+import { resolvers } from "./graphql/resolvers";
+
+console.log(resolvers);
 
 createConnection()
   .then(async (connection) => {
@@ -17,8 +22,19 @@ createConnection()
       res.send("Hello, World!");
     });
 
+    // setup ApolloServer
+    const apolloServer = new ApolloServer({
+      typeDefs,
+      resolvers,
+    });
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app });
+
+    // listening to port
     app.listen(CONSTANTS.PORT, () =>
-      console.log(`Server running on http://localhost:${CONSTANTS.PORT}`)
+      console.log(
+        `Server running on http://localhost:${CONSTANTS.PORT}/graphql`
+      )
     );
   })
   .catch((error) => console.log(error));
