60a8c7d0eea78aa151ad5f65a66aaccd786a19a1
PrinceNarteh <prinarteh@gmail.com>
2021-12-19 20:26:04 +0000

    adds signIn mutation

--- server/src/graphql/user.resolver.ts
+++ server/src/graphql/user.resolver.ts
@@ -11,8 +11,7 @@ export class UserResolver {
   }
 
   @Mutation(() => User)
-  async signUp(@Arg("data") data: UserInput): Promise<User> {
-    const { email, password } = data;
+  async signUp(@Arg("data") { email, password }: UserInput): Promise<User> {
     try {
       const userExists = await User.findOne({ where: { email } });
       if (userExists) throw new Error("User with this email already exists");
@@ -23,6 +22,22 @@ export class UserResolver {
         username: email.split("@")[0],
       });
       await user.save();
+      return user;
+    } catch (error: any) {
+      throw new Error(error.message);
+    }
+  }
+
+  @Mutation(() => User)
+  async signIn(@Arg("data") { email, password }: UserInput): Promise<User> {
+    try {
+      const user = await User.findOne({ where: { email } });
+      if (!user) throw new Error("Invalid credentials");
+
+      if (!(await bcrypt.compare(password, user.password))) {
+        throw new Error("Invalid credentials");
+      }
+
       return user;
     } catch (error: any) {
       throw new Error(error.message);
