5ba6cb64d90bc052520b5724a4a1373cc19816bf
PrinceNarteh <prinarteh@gmail.com>
2021-12-29 08:02:10 +0000

    fix isAuth bug

--- server/src/middleware/isAuth.ts
+++ server/src/middleware/isAuth.ts
@@ -3,16 +3,24 @@ import jwt from "jsonwebtoken";
 import { MyContext } from "../helpers/myContext";
 import { CONSTANTS } from "../constants/strings";
 
+interface JwtPayload {
+  userId: string;
+}
+
 export const isAuth: MiddlewareFn<MyContext> = async ({ context }, next) => {
   try {
     const bearer = context.req.headers["authorization"];
     const token = bearer?.split(" ")[1];
     if (!token) throw new Error("Not authenticated");
 
-    const tokenPayload = jwt.verify(token, CONSTANTS.ACCESS_TOKEN_SECRET);
+    const tokenPayload = jwt.verify(
+      token,
+      CONSTANTS.ACCESS_TOKEN_SECRET
+    ) as JwtPayload;
     if (!tokenPayload) throw new Error("Not authenticated");
-    context.tokenPayload = tokenPayload as any;
+    context.tokenPayload = tokenPayload;
   } catch (error) {
     throw new Error("Not authenticated");
   }
+  return next();
 };
